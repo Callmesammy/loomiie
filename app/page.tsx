a279206763d2a8ec3eaa5d3d350c0afac2cfa534
Callmesammy <callmesammy@yahoo.com>
2026-08-06 00:46:47 +0100

    save

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { HeroSection } from "@/components/HeroSection";
 import { Marquee } from "@/components/Marquee";
 import { PortfolioGrid } from "@/components/PortfolioGrid";
 import { Footer } from "@/components/Footer";
-
+  
 export default function Home() {
   return (
     <main className="relative min-h-screen bg-background text-foreground overflow-hidden">
