088a2c7830a1fc346913ee5918680e0ffcdc3514
Callmesammy <91014957+Callmesammy@users.noreply.github.com>
2026-08-06 22:49:38 +0100

    feat: implement initial site pages including work case studies, contact, identity, and landing views

--- app/values/page.tsx
+++ app/values/page.tsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Navbar } from "@/components/Navbar";
-import { ValuesSection } from "@/components/ValuesSection";
-import { Footer } from "@/components/Footer";
-import { Preloader } from "@/components/Preloader";
+import { Navbar } from "../../components/Navbar";
+import { ValuesSection } from "../../components/ValuesSection";
+import { Footer } from "../../components/Footer";
+import { Preloader } from "../../components/Preloader";
 import { Metadata } from "next";
 
 export const metadata: Metadata = {
